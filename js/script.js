7c87ed5ad2925dfd7ce8d3d09c65c9362e120456
Anika Tabassum <anikatabassum2738@gmail.com>
2026-07-31 15:13:35 +0600

    Add client section with testimonials and update navbar background color

--- js/script.js
+++ js/script.js
@@ -8,3 +8,14 @@ document.addEventListener('DOMContentLoaded', function () {
     });
 });
 
+// change navbar background on scroll
+window.addEventListener('scroll', function () {
+    const navbar = document.querySelector('.navbar');
+
+    if (window.scrollY > 0) {
+        navbar.classList.add('navbar-scroll');
+    }
+    else {
+        navbar.classList.remove('navbar-scroll');
+    }
+});
